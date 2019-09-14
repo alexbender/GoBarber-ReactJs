cb39c0a037d44f1770a09742a884c314211b1e27
Alex Mariot Bender <alexbenderkv@gmail.com>
2019-09-13 21:23:15 -0300

    Gobarber Web

--- gobarber/src/App.js
+++ gobarber/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
+import { Router } from 'react-router-dom';
+import './config/ReactotronConfig';
+import history from './services/history';
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>H W</h1>
-    </div>
+    <Router history={history}>
+      <Routes />
+      <GlobalStyle />
+    </Router>
   );
 }
 
